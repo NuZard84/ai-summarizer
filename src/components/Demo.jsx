0a791397631755292de994a025ec1ccf243a32ed
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2023-04-22 16:42:40 +0530

    basic

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import { linkIcon } from "../assets/index";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handlSubmit = async (e) => {
+    e.preventDefault();
+    alert("Please enter a valid URL");
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col gap-2 w-full">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={handlSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icn"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↵
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,31 @@
+import { logo } from "../assets/index";
+
 const Hero = () => {
-  return <div>hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          className="black_btn"
+          onClick={() =>
+            window.open("https://github.com/NuZard84/ai-summarizer")
+          }
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAi GPT</span>
+      </h1>
+      <h2 className="desc">
+        Effortlessly distill large volumes of text into concise summaries with
+        our AI summarizer powered by OpenAI&#39;s state-of-the-art natural
+        language processing technology.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
